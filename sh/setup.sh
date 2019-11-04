e81468ae03dbd44a656de341fb76b3681a115a04
gkzz <gakuji.tamaki@gmail.com>
2019-11-05 00:25:58 +0900

    mod sh/setup.sh

--- sh/setup.sh
+++ sh/setup.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+function help {
+  cat <<- EOF
+    overview：wheter run "docker-compose up -d --build", or not
+    usage：bash sh/setup.sh [-h|-b|-n|
+    option：
+      -h  this message
+      -b ->> run "docker-compose up -d --build", after removing all contrainers
+      -n ->> run "nothing" after removing them
+EOF
+    exit 1
+
+}
+
+while getopts ":bn" OPT;
+
+do
+    case ${OPT} in
+        b)
+          BUILD_FLAG=1;
+            ;;
+        n)
+          BUILD_FLAG=0;
+            ;;
+        \?)
+          help
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
 IDS=$(docker container ls -aq)
 
 for i in $IDS
@@ -20,4 +51,14 @@ docker image prune --force
 docker volume prune --force
 
 docker-compose down -v
-docker-compose up -d --build
+
+if [ "$BUILD_FLAG" -eq 1  ]; then
+  echo 'Run "docker-compose up -d --build"'
+  docker-compose up -d --build
+  bash sh/curlHttp.sh
+elif [ "$BUILD_FLAG" -eq 0 ]; then
+  echo 'Not Run "docker-compose up -d --build"'
+else
+  echo "option is unexpected."
+  help
+fi
