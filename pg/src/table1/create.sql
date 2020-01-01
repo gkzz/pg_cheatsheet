4c7e21f30589fb9e7aabb989a8f08b20dc0d8af5
gkzz <gakuji.tamaki@gmail.com>
2020-01-01 15:55:54 +0900

    mv

---
+++ pg/src/table1/create.sql
@@ -0,0 +1,5 @@
+create table table1 (
+    id     char(4)  primary key,
+    name   text     not null unique,
+    age    integer
+);
---
+++ pg/src/table2/create.sql
@@ -0,0 +1,9 @@
+create table table2 (
+    id      integer primary key,
+    name    text    not null,
+    height  integer not null,
+    gender  char(1) not null
+);
+
+
+      
