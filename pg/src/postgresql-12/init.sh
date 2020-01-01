4752d00516532ba414d650d6cf81421f829f7639
gkzz <gakuji.tamaki@gmail.com>
2020-01-01 15:38:38 +0900

    rename sql postgresql-12

---
+++ pg/src/postgresql-12/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+username=tempuser1 \
+&& dbname=tempdb1 \
+&& createuser -U postgres $username \
+&& createdb -U postgres -O \
+$username -E UTF8 --locale=C -T template0 $dbname \
+&& psql -U $username $dbname
+
+
