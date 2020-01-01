fd78399b8d09e0097813a64c4d817c379266b325
gkzz <gakuji.tamaki@gmail.com>
2020-01-01 16:16:20 +0900

    mod

--- pg/src/init.sh
+++ pg/src/init.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-username=tempuser1 \
-&& dbname=tempdb1 \
-&& createuser -U postgres $username \
-&& createdb -U postgres -O \
-$username -E UTF8 --locale=C -T template0 $dbname \
-&& psql -U $username $dbname
+username=tempuser1
+dbname=tempdb1
 
-
+createuser -U postgres $username
+createdb -U postgres -O $username -E UTF8 --locale=C -T template0 $dbname
+psql -U $username $dbname
